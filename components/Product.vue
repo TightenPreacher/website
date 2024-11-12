30d2da1ac702b1b547e0653a2f26e8b4dc839425
面包 <731995484@qq.com>
2024-11-13 02:35:06 +0800

    [M] 产品页面基本组件

--- components/Product.vue
+++ components/Product.vue
@@ -1,11 +1,19 @@
 <template>
-    <div class="carousel-item w-full">
-        <Mod />
+    <div class="carousel-item w-full flex flex-col">
+        <!-- <Mod /> -->
+         <World />
+         <Skill />
+         <Robot />
+         <System />
     </div>
 </template>
 
 <script setup lang="ts">
 import Mod from '@/components/Product/Mod.vue'
+import World from '@/components/Product/World.vue'
+import Skill from '@/components/Product/Skill.vue'
+import Robot from '@/components/Product/Robot.vue'
+import System from '@/components/Product/System.vue'
 
 </script>
 
---
+++ components/Product/Skill.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="h-[40.09vw]">
+        <div class="h-[3.07vw] text-[2.19vw] leading-[2.55vw] font-semibold text-black mt-[13.44vw]">联系我们</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
