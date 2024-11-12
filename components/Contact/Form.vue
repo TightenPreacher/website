d8acd61bf51ecc17b1abe8f67de56fbe61286e22
hejiachi <731995484@qq.com>
2024-11-12 09:57:04 +0800

    [M] 联系我们，表单文案

--- components/Contact/Form.vue
+++ components/Contact/Form.vue
@@ -3,14 +3,36 @@
         <div class="h-[3.07vw] text-[2.19vw] leading-[2.55vw] font-semibold text-[#030303]">项目合作</div>
         <div class="mt-[1.56vw]">
             <div class="h-[2.71vw] text-[1.25vw] leading-[2.71vw] font-normal text-[#555555]">公司/机构/组织名称*</div>
-            <div></div>
+            <div>
+                <input type="text" placeholder="请输入" class="input input-bordered w-full max-w-xs" />
+            </div>
         </div>
-        <div class="mt-[1.56vw]">2</div>
+        <div class="mt-[1.56vw] flex justify-between">
+            <div>
+                <div class="h-[2.71vw] text-[1.25vw] leading-[2.71vw] font-normal text-[#555555]">联系人姓名*</div>
+                <input type="text" placeholder="请输入" class="input input-bordered w-full max-w-xs" />
+            </div>
+            <div>
+                <div class="h-[2.71vw] text-[1.25vw] leading-[2.71vw] font-normal text-[#555555]">联系人电话*</div>
+                <input type="text" placeholder="请输入" class="input input-bordered w-full max-w-xs" />
+            </div>
+            <div>
+                <div class="h-[2.71vw] text-[1.25vw] leading-[2.71vw] font-normal text-[#555555]">联系人邮箱*</div>
+                <input type="text" placeholder="请输入" class="input input-bordered w-full max-w-xs" />
+            </div>
+        </div>
         <div class="mt-[1.56vw]">
             <div class="h-[2.71vw] text-[1.25vw] leading-[2.71vw] font-normal text-[#555555]">意向合作方式</div>
-            <div></div>
+            <div class="flex justify-between">
+                <div>研发合作</div>
+                <div>市场合作</div>
+                <div>项目资助</div>
+                <div>其他</div>
+            </div>
         </div>
-        <div class="mt-[2.6vw]">4</div>
+        <div class="mt-[2.6vw]">
+            <div>提交</div>
+        </div>
     </div>
 </template>
 
