f3b8dd478d9b6b90536d0cdc6ca66b0938f427cf
面包 <731995484@qq.com>
2024-11-30 16:14:53 +0800

    [M] 添加回到顶部按钮

--- pages/news.vue
+++ pages/news.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="overflow-hidden overflow-y-auto relative h-[100vh] family">
+    <div class="overflow-hidden overflow-y-auto relative h-[100vh] family" ref="contentRef" @scroll="handleScroll">
         <div id="mains" class="my-0 mx-auto w-full relative">
             <!-- <div id="mains" class="my-0 mx-auto w-[62.5vw]"> -->
             <Header :type="'news'" />
@@ -10,7 +10,21 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+const { $eventBus } = useNuxtApp();
 
+const contentRef = ref(null);
+
+const handleScroll = () => {
+    if (contentRef.value) {
+        const scrollTop = contentRef.value.scrollTop;
+        if (scrollTop > (window.innerHeight)*2) {
+            $eventBus.emit('scroll', true);
+        } else {
+            $eventBus.emit('scroll', false);
+        }
+    }
+}
 </script>
 
 <style scoped>
